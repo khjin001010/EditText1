f20da49dc0dcfcd3043cc395ad764b7efca874df
khjin001010 <khjin001010@naver.com>
2017-03-23 20:57:12 +0900

    #2 event

--- app/src/main/java/khjin001010/kr/hs/emirim/edittext1/MainActivity.java
+++ app/src/main/java/khjin001010/kr/hs/emirim/edittext1/MainActivity.java
@@ -2,6 +2,10 @@ package khjin001010.kr.hs.emirim.edittext1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText myedit = (EditText)findViewById(R.id.edit_1);
+        Button mybutton = (Button) findViewById(R.id.button);
+        final String myStr = myedit.getText().toString();
+
+        final TextView text_1 = (TextView) findViewById(R.id.txt_1);
+
+        mybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_1.setText(myStr);
+            }
+        });
+
+
     }
 }
